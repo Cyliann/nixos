95534d06a392f48a32c5af059ee8b9fe2f4d1c65
Cyliann <maksymilian@cych.eu>
2024-07-01 12:07:26 +0200

    Add disko config to host/server

---
+++ hosts/server/disko.nix
@@ -0,0 +1,48 @@
+{
+  disko.devices = {
+    disk = {
+      vdb = {
+        type = "disk";
+        device = "/dev/disk/by-diskseq/1";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              start = "1M";
+              end = "128M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # Override existing partition
+                subvolumes = {
+                  "/root" = {
+                    mountpoint = "/";
+                  };
+                  "/home" = {
+                    mountpoint = "/home";
+                  };
+                  "/nix" = {
+                    mountpoint = "/nix";
+                  };
+                  "/media" = {
+                    mountpoint = "/media";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
