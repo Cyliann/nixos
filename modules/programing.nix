8364929502d258eb301b21a22891c12bb51b77c5
Cyliann <maksymilian@cych.eu>
2024-09-02 10:15:20 +0200

    Add jupyter and themes

--- modules/programing.nix
+++ modules/programing.nix
@@ -8,8 +8,10 @@
     killall
     (python312.withPackages (ps: with ps; [ # install python 3.12 and libraries
       requests
+      jupyterthemes
     ]))
     poetry # python library management
+    jupyter
   ];
     
   fonts.packages = with pkgs; [
