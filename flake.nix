31b0bbcc995a2639619bccab07bbd234dcca48d7
Cyliann <maksymilian@cych.eu>
2025-10-26 20:36:36 +0100

    feat: cachix support

--- flake.nix
+++ flake.nix
@@ -29,6 +29,21 @@
     };
   };
 
+  # Cachix support
+  nixConfig = {
+    # override the default substituters
+    substituters = [
+      "https://cache.nixos.org"
+
+      # nix community's cache server
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      # nix community's cache server public key
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   outputs = {nixpkgs, ...} @ inputs: let
     system = "x86_64-linux";
   in {
