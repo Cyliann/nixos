4064e98c62959f7b89695e9558c56d9354465906
Cyliann <maksymilian@cych.eu>
2025-11-09 16:02:28 +0100

    remove fcitx

--- modules/nixos/base-config.nix
+++ modules/nixos/base-config.nix
@@ -26,15 +26,6 @@
       LC_TELEPHONE = "pl_PL.utf8";
       LC_TIME = "pl_PL.utf8";
     };
-    inputMethod = {
-      enable = true;
-      type = "fcitx5";
-      fcitx5.addons = with pkgs; [
-        fcitx5-mozc
-        fcitx5-gtk
-        fcitx5-chinese-addons
-      ];
-    };
   };
   console.keyMap = "pl";
 
